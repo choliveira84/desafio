12199ec5ef0eb5a467ff925085ebecc7b93597c0
cho <cho@cesar.org.br>
2019-04-16 15:43:56 -0300

    heroku

--- src/main/java/com/pitang/desafio/api/model/entity/Usuario.java
+++ src/main/java/com/pitang/desafio/api/model/entity/Usuario.java
@@ -8,18 +8,18 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
+import org.hibernate.annotations.GenericGenerator;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -39,75 +39,78 @@ import lombok.ToString;
 @ToString
 @Entity
 @EntityListeners(AuditingEntityListener.class)
-@JsonIgnoreProperties(value = { "created_at", "last_login" }, allowGetters = true)
+@JsonIgnoreProperties(value = {
+    "created_at", "last_login"
+}, allowGetters = true)
 
 public class Usuario implements Serializable, UserDetails {
 
-    private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    protected Long id;
+  @Id
+  @GeneratedValue(generator = "increment")
+  @GenericGenerator(name = "increment", strategy = "increment")
+  protected Long id;
 
-    /**
-     * The 'e-mail' field is working as 'username'
-     */
-    @Email(message = "Invalid fields")
-    @NotBlank(message = "Missing fields")
-    @Column(nullable = false, unique = true)
-    private String email;
+  /**
+   * The 'e-mail' field is working as 'username'
+   */
+  @Email(message = "Invalid fields")
+  @NotBlank(message = "Missing fields")
+  @Column(nullable = false, unique = true)
+  private String email;
 
-    @NotNull(message = "Invalid Fields")
-    @NotBlank(message = "Missing fields")
-    @Column(nullable = false)
-    private String password;
+  @NotNull(message = "Invalid Fields")
+  @NotBlank(message = "Missing fields")
+  @Column(nullable = false)
+  private String password;
 
-    @NotNull(message = "Invalid Fields")
-    @NotBlank(message = "Missing fields")
-    @Column(nullable = false)
-    private String firstName;
+  @NotNull(message = "Invalid Fields")
+  @NotBlank(message = "Missing fields")
+  @Column(nullable = false)
+  private String firstName;
 
-    @NotNull(message = "Invalid Fields")
-    @NotBlank(message = "Missing fields")
-    @Column(nullable = false)
-    private String lastName;
+  @NotNull(message = "Invalid Fields")
+  @NotBlank(message = "Missing fields")
+  @Column(nullable = false)
+  private String lastName;
 
-    @CreatedDate
-    @Column(name = "created_at", nullable = false, updatable = false)
-    private LocalDateTime created_at;
+  @CreatedDate
+  @Column(name = "created_at", nullable = false, updatable = false)
+  private LocalDateTime created_at;
 
-    @CreatedDate
-    @Column(name = "last_login", nullable = false, updatable = true)
-    private LocalDateTime last_login;
+  @CreatedDate
+  @Column(name = "last_login", nullable = false, updatable = true)
+  private LocalDateTime last_login;
 
-    @Override
-    public String getUsername() {
-        return getEmail();
-    }
+  @Override
+  public String getUsername() {
+    return getEmail();
+  }
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
-    }
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return null;
+  }
 
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
 
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
 
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
 
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
 
 }
